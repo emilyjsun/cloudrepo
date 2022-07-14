75c68d14e3401d457eeb0fddaf8fc310d3caa5ec
Emily Sun <ejs83@duke.edu>
2022-07-14 10:29:26 -0400

    added navbar comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navigationbar from "./Components/Navigationbar.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 
 function App() {
   return (
     <div className="App">
+      <Navigationbar></Navigationbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <a
           className="App-link"
@@ -15,7 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World
         </a>
       </header>
     </div>
